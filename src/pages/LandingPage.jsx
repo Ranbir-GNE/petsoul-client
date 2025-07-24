cbb78244d3612cbc1c4a609f6e85bbebad1e900a
Ranbir <ranbir2115113@gndec.ac.in>
2025-07-24 16:00:53 +0530

    the navbar was overflowing on the Landing Page while viewing on the mobile. fixed that.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,7 +6,7 @@ import Footer from "@/components/dashboard/Footer";
 const LandingPage = () => {
   return (
     <div className="flex flex-col min-h-screen bg-black/90">
-      <div className="min-w-[1140px] mx-auto">
+      <div className="w-full max-w-[1140px] mx-auto px-4">
         <Navbar />
       </div>
 
